7825c3d6a8867ad2cc0353b955755a99b20166f1
ackermanjayjay <reza23198@gmail.com>
2022-11-25 19:08:26 +0700

    fixing tampilan gambar, serta ingin menambahkan detail pada saat menekan button for detail

--- server.py
+++ server.py
@@ -5,6 +5,14 @@ app=Flask(__name__)
 
 
 
+@app.route('/<int:id>',methods=['GET','POST'])
+def detail_data(id):
+       if request.method == 'GET':
+        crawl_text=req.get("https://fakestoreapi.com/products")
+        data_crawl=crawl_text.json()
+        keterangan_data=data_crawl(id)
+        return render_template('index.html/id',datas=keterangan_data)
+
 @app.route('/')
 def index():
     crawl_text=req.get("https://fakestoreapi.com/products")
@@ -12,14 +20,6 @@ def index():
     keterangan_data=data_crawl
   
     return render_template('index.html',datas=keterangan_data)
-
-@app.route('/data')
-def ambil_data():
-    crawl_text=req.get("https://fakestoreapi.com/products")
-    data_crawl=crawl_text.json()
-    keterangan_data=data_crawl
-  
-    return render_template('data.html',datas=keterangan_data)
     
 
 if __name__=='__main__':
