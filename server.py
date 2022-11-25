88c88a8ae6f065ca3b1ce102fad97520a009ebb9
ackermanjayjay <reza23198@gmail.com>
2022-11-25 15:53:12 +0700

    adding tampilan depan di index untuk tampilan gambar

--- server.py
+++ server.py
@@ -6,12 +6,20 @@ app=Flask(__name__)
 
 
 @app.route('/')
-def ambil_data():
+def index():
     crawl_text=req.get("https://fakestoreapi.com/products")
     data_crawl=crawl_text.json()
     keterangan_data=data_crawl
   
     return render_template('index.html',datas=keterangan_data)
+
+@app.route('/data')
+def ambil_data():
+    crawl_text=req.get("https://fakestoreapi.com/products")
+    data_crawl=crawl_text.json()
+    keterangan_data=data_crawl
+  
+    return render_template('data.html',datas=keterangan_data)
     
 
 if __name__=='__main__':
